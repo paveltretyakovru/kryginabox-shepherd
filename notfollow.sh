8bd463496a3543a4a4efcb2a04424cfc04b8e22d
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-06-27 14:53:05 +0300

    Create README.md; Update notfollow.sh

--- notfollow.sh
+++ notfollow.sh
@@ -1,5 +1,5 @@
 # Не следить за изменениями в файлах
 git update-index --assume-unchanged ./functions/authData.py
 echo 'Отключена git слежка за файлом ./functions/authData.py (данные ВК)'
-git update-index --assume-unchanged ./script.sh
-echo 'Отключена слежка за изменениями в файле ./script.py (в нем указан путь до скрипта)'
+git update-index --assume-unchanged ./crontask.sh
+echo 'Отключена слежка за изменениями в файле ./crontask.py (в нем указан путь до скрипта)'
